f4968047fcace68ca67a0f10168047cca0597f30
Gabriel Rodrigues <gabrielrodrigues1114@gmail.com>
2024-06-06 20:17:11 -0300

    Update Front-End

--- src/AgendaIBI/wwwroot/js/script.js
+++ src/AgendaIBI/wwwroot/js/script.js
@@ -14,9 +14,25 @@ function getUserRole() {
     return null;
 }
 
+function checkAuthorization(allowedRoles) {
+    const role = getUserRole();
+    if (!role || !allowedRoles.includes(role)) {
+        // Redireciona para a página inicial se o usuário não estiver autorizado
+        window.location.href = "https://localhost:7247/index.html";
+        return false;
+    }
+    return true;
+}
+
 // Função para renderizar a UI com base na role do usuário
 function renderUI() {
     const role = getUserRole();
+
+    if (!role) {
+        // Caso não consiga obter o papel (role) do usuário
+        window.location.href = "https://localhost:7247/index.html";
+        return;
+    }
 
     // Verificar a role do usuário e ocultar elementos de acordo
     switch (role) {
@@ -39,21 +55,24 @@ function renderUI() {
             });
             break;
         default:
-            // Caso o papel (role) do usuário não seja reconhecido
-            window.location = "https://localhost:7247/index.html";
             break;
     }
 }
 
-const botaoLogout = document.getElementById('logout')
-botaoLogout.addEventListener("click", function(e){
-    e.preventDefault();
-    
+document.addEventListener("DOMContentLoaded", function() {
+    const botaoLogout = document.getElementById('logout');
+    if (botaoLogout) {
+        botaoLogout.addEventListener("click", function(e){
+            e.preventDefault();
+            localStorage.removeItem("jwtToken");
+            window.location.href = "https://localhost:7247/index.html";
+        });
+    } else {
+        console.error("Elemento com ID 'logout' não encontrado.");
+    }
 
-})
-
-
-// Chame renderUI quando a página for carregada
-document.addEventListener('DOMContentLoaded', function() {
-    renderUI();
+    if (checkAuthorization(allowedRoles)) {
+        // Chame renderUI quando a página for carregada e o usuário estiver autorizado
+        renderUI();
+    }
 });
